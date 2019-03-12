34060c66cc117daa5949eedc8544eb13ea047924
YazanZebak <yazanzk16@gmail.com>
2019-03-12 22:47:32 +0200

    Update Spoj IOPC1207

--- Spoj/IOPC1207.cpp
+++ Spoj/IOPC1207.cpp
@@ -4,80 +4,93 @@
 #include <bits/stdc++.h>
 typedef long long ll ;
 using namespace std;
- 
+
 const int N = 100005 ;
-ll tree[N << 2][3] ;
-ll lazy[N << 2][3] ;
-ll nx , ny , nz , n ;
- 
+struct node{
+     ll red , lazy , green ;
+     node(ll red , ll lazy , ll green) : red(red) , lazy(lazy) , green(green) {}
+     node() {red = lazy = green = 0 ;}
+} tree[N << 2][3];
+
+
+void build(int s , int e , int id ,  int p = 1){
+     if(s == e){
+          tree[p][id] = node(0 , 0 , 1);
+          return ;
+     }
+     build(s , (s + e)/2 , id , 2 * p);
+     build((s + e)/2 + 1 , e , id , 2 * p + 1);
+     tree[p][id].green = tree[2 * p][id].green + tree[2 * p + 1][id].green ;
+}
+
 void pushLazy(int s , int e , int p , int dim){
-     if(lazy[p][dim]){
-           tree[p][dim] ^= 1  ;
+     if(tree[p][dim].lazy){
+           swap(tree[p][dim].green , tree[p][dim].red);
            if(s != e){
-                 lazy[2 * p][dim] ^= 1;
-                 lazy[2 * p + 1][dim] ^= 1 ;
+                 tree[2 * p][dim].lazy ^= 1;
+                 tree[2 * p + 1][dim].lazy ^= 1 ;
            }
-           lazy[p][dim] = 0 ;
+           tree[p][dim].lazy = 0 ;
      }
 }
- 
-void update(int l , int r , int dim , int s = 0 , int e = n - 1 , int p = 1){
- 
-     if(s > r || e < l) return ;
- 
+
+void update(int l , int r , int dim , int s , int e  , int p = 1){
+
      pushLazy(s , e , p , dim);
- 
+
+     if(s > r || e < l) return ;
+
      if(s >= l && e <= r){
-            tree[p][dim] ^= 1 ;
-            if(s != e){
-                   lazy[2 * p][dim] ^= 1 ;
-                   lazy[2 * p + 1][dim] ^= 1 ;
-            }
+            tree[p][dim].lazy ^= 1 ;
+            pushLazy(s, e , p , dim);
             return ;
      }
- 
+
      update(l , r , dim , s , (s + e)/2 , 2 * p) ;
      update(l , r , dim , (s + e)/2 + 1 , e , 2 * p + 1);
-     tree[p][dim] = tree[2 * p][dim] + tree[2 * p + 1][dim];
+     tree[p][dim].red = tree[2 * p][dim].red + tree[2 * p + 1][dim].red;
+     tree[p][dim].green = tree[2 * p][dim].green + tree[2 * p + 1][dim].green;
 }
- 
-ll query(int l , int r , int dim , int s = 0 , int e = n - 1 , int p = 1){
-    if(s > r || e < l) return 0 ;
+
+ll query(int l , int r , int dim , int s , int e , int p = 1){
     pushLazy(s , e , p , dim) ;
+    if(s > r || e < l) return 0 ;
     if(s >= l && e <= r)
-          return tree[p][dim] ;
+          return tree[p][dim].red ;
     return query(l , r , dim , s , (s + e)/2 , 2 * p) + query(l , r , dim , (s + e)/2 + 1 , e , 2 * p + 1) ;
 }
- 
+
 int main()
 {
     ios::sync_with_stdio(false) , cin.tie(0) , cout.tie(0) ;
- 
+
     int t ; cin >> t;
- 
+
     while(t--){
           memset(tree , 0 , sizeof tree);
-          memset(lazy , 0 , sizeof lazy);
- 
+
+
           int q;
+          int nx , ny , nz , n ;
           cin >> nx >> ny >> nz >> q ;
- 
+
+          build(0 , nx - 1 , 0);
+          build(0 , ny - 1 , 1);
+          build(0 , nz - 1 , 2);
+
           while(q--){
                  int type ; cin >> type ;
                  if(type == 3){
                     ll lx , ly , lz , rx , ry , rz ;
                     cin >> lx >> ly >> lz >> rx >> ry >> rz ;
-                    n = nx ;
-                    ll redx = query(lx , rx , 0) ;
-                    n = ny ;
-                    ll redy = query(ly , ry , 1) ;
-                    n = nz ;
-                    ll redz = query(lz , rz , 2) ;
- 
+                    ll redx = query(lx , rx , 0 , 0 , nx - 1) ;
+                    ll redy = query(ly , ry , 1 , 0 , ny - 1) ;
+                    ll redz = query(lz , rz , 2 , 0 , nz - 1) ;
+
                     ll grex = (rx - lx + 1) - redx ;
                     ll grey = (ry - ly + 1) - redy ;
-                    ll grez = (rz - lz + 1) - redz  ;
- 
+                    ll grez = (rz - lz + 1) - redz ;
+
                     ll res = (redx * redy * redz) + (redx * grey * grez) + (grex * redy * grez) + (grex * grey * redz) ;
                     cout << res << endl ;
                  }
@@ -87,15 +100,12 @@ int main()
                       if(type == 2) n = nz ;
                       ll l , r ;
                       cin >> l >> r ;
-                      update(l , r , type) ;
+                      update(l , r , type , 0 , n - 1) ;
                  }
           }
- 
- 
+
+
     }
- 
- 
- 
- 
+
     return 0 ;
 }
